3ec8bb32c83b77860819ceb62eb7714d0badf8b7
Wittawas Nakkasem <vittee@hotmail.com>
2015-07-27 23:25:06 +0700

    Minor code tweaks.

--- background.js
+++ background.js
@@ -1,24 +1,25 @@
-// Blognone already use jquery 1.2 so we cannot use $.closest()
+(function($) {
+  var patterns = [
+    '=>',
+    // '->', // this pattern is less likely to be bot
+    'หัวข้อข่าว',
+  ];
 
-var patterns = [
-  '=>',
-  // '->', // this pattern is less likely to be bot
-  'หัวข้อข่าว',
-];
+  var usernames = [
+    'panurat2000'
+  ];
 
-var usernames = [
-  'panurat2000'
-];
+  [].concat(
+    patterns.map(function(p) {
+      return $('.comment-content:contains("' + p + '")').parent('.comment-body');
+    }),
 
-$.each(patterns, function(index, pattern) {
-  markDomAsBot($('.comment-content:contains("'+pattern+'")').parent('.comment-body'));
-});
+    usernames.map(function(username) {
+      return $('.comment-info:contains("' + username + '")').siblings('.comment-body');
+    })
+  )
+  .forEach(function(dom) {
+    dom.hide().siblings('.comment-info').css({background: '#7D7D7D'});
+  });
 
-$.each(usernames, function(index, username) {
-  markDomAsBot($('.comment-info:contains("'+username+'")').siblings('.comment-body'));
-});
-
-function markDomAsBot(dom) {
-  dom.hide();
-  dom.siblings('.comment-info').css({background: '#7D7D7D'});
-}
+})(jQuery);
